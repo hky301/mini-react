342c2d6324c77dbe66a25e68a87f80292a7ccb78
HeKaiYu <hekaiyu_301@163.com>
2024-01-19 09:18:15 +0800

    chore: 切换显示，显示在浏览器上的dom 结构变了

--- vite-runner/App.jsx
+++ vite-runner/App.jsx
@@ -33,20 +33,37 @@ function Bar() {
 
 
 let currentRoot = 1
+
+function Child1() {
+  return <div id='child1'>child1
+    <p>aaaa</p>
+  </div>
+}
+
+function Child2() {
+  return <div id='child2'>Child2</div>
+}
+
+let isShow = false
+
 function App() {
   console.log('app');
   const update = React.update()
   function handleClick() {
     currentRoot++
+    isShow = !isShow
     update()
   }
 
   return <div id='app'>
     <div>hi,mini-react</div>
     <div>root: {currentRoot}</div>
+    {/* TODO:切换显示，child 会到dom最后面，估计fiber结构变了 */}
+    {isShow ? <Child1 /> : <Child2 />}
     <button onClick={handleClick}>点击</button>
     <Foo />
     <Bar />
+    1
   </div>
 }
 
